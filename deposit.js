47fbf6ebf156c9f64cfde6dd94d30f732a903cd8
Madison Hill <madisonhill@macbook-pro.attlocal.net>
2022-04-15 14:10:01 -0400

    adding

---
+++ deposit.js
@@ -0,0 +1,10 @@
+let userInput= prompt("If you would like to Deposit Money Enter 1 or if you would like to Withdraw money enter 2 ");
+
+
+if (userInput== "1") {
+    let userInput= prompt( "How much would you like to deposit?")
+}
+
+if(userInput=="2"){
+    let userInput=  prompt( "How much would you like to withdraw?")
+}
